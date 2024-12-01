910595a9930d72fbcba428977f03ef5e0ae80005
Gurug1408 <charang1810@gmail.com>
2024-12-01 22:18:28 +0000

    Updated the Class 03 concepts

---
+++ Introduction/k_identifier_casing.py
@@ -0,0 +1,37 @@
+"""
+Purpose: Identifier Casing - class names should be CamelCase, identifier names should be in snake( or Underscore) case .
+
+"""
+
+# Python - case-sensitive language
+animal = "CAT"
+print(animal)
+
+# print(Animal) - NameError: name 'Animal' is not defined.
+
+ANIMAL = "RAT"
+print(ANIMAL)
+
+Animal = "Goat"
+print(Animal)
+
+# variable casing
+# 1.snake casing or underscore casing
+
+student = "charan"
+employee_salary = 453876.12
+cost_of_mango = 10
+selling_price_of_apples = 25
+
+output_of_thermal_sensor = 32
+no_of_current_processes = 5
+
+
+# 2. Camel casing
+Student = "Guru"
+EmployeeSalary = 453876.12
+CostOfMango = 10
+SellingPriceOfApples = 25
+
+OutputOfThermalSensor = 32
+NoOfCurrentProcesses = 5
---
+++ Introduction/m_semicolumn_operator.py
@@ -0,0 +1,20 @@
+"""
+Purpose: Multiple statements in same line - , logic separator and ; statement completion operator
+"""
+print("Hello" "world")
+print("Hello", "world")
+
+print("Hello", 21312)
+
+print("Hello", 21312, 213, 123 + 123 - 3)
+print()
+
+
+# semicoln operator
+num1 = 100
+num2 = 200
+sum_of_numbers = num1 + num2
+print("Sum of Number:", sum_of_numbers)
+
+# operator
+num1 = 100; num2 = 200; sum_of_numbers = num1 + num2; print("Sum of Number:", sum_of_numbers)
